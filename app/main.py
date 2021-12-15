778ee4fb2cfa8a57a15c22fa5a6a62c27feb34d9
hoimas <b061vdv@yamaguchi-u.ac.jp>
2021-12-15 13:58:52 +0000

    add mainにplantルーターを追加

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
-from app.routers import user#, plant, home, timeline, character
+from app.routers import plant, user  # , home, timeline, character
 
 app = FastAPI()
 app.include_router(user.router)
-# app.include_router(plant.router)
+app.include_router(plant.router)
 # app.include_router(home.router)
 # app.include_router(timeline.router)
 # app.include_router(character.router)
