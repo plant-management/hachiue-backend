2348c77b3cf6232c414d491f3ead03d31d91e936
hoimas <b061vdv@yamaguchi-u.ac.jp>
2021-12-15 15:01:54 +0000

    fix 昨日に関するコードも見やすさを修正

--- app/func/img_func.py
+++ app/func/img_func.py
@@ -1,7 +1,10 @@
-import pytz, base64, uuid
-from PIL import Image
+import base64
+import uuid
 from ctypes import resize
 from datetime import datetime
+
+import pytz
+from PIL import Image
 
 
 # #経過日数取得用[関数]
@@ -13,21 +16,23 @@ def calc_progress_day(time1):
     return day
 
 
-#png→base64に変換
+# png→base64に変換
 def png_to_base64(img_path):
-    img_resize = Image.open(img_path).resize((150,150)) #ここ処理は取得時に行う
-    img_resize.save(img_path)                           #ここ処理は取得時に行う
-    img_data = open(img_path,"rb").read()
-    b64_data = base64.b64encode(img_data).decode('utf-8')
+    img_resize = Image.open(img_path).resize((150, 150))  # ここ処理は取得時に行う
+    img_resize.save(img_path)  # ここ処理は取得時に行う
+    img_data = open(img_path, "rb").read()
+    b64_data = base64.b64encode(img_data).decode("utf-8")
     return b64_data
+
+
 # print(png_to_base64("test1.png"))
 
 
 # base64→pngに変換
-def b64_to_png(Base64_image, UPLOAD_FOLDER):    
-    #character画像の保存先パス
-    plant_image_path = UPLOAD_FOLDER + str(uuid.uuid4()) + ".png" 
+def b64_to_png(Base64_image, UPLOAD_FOLDER):
+    # character画像の保存先パス
+    plant_image_path = UPLOAD_FOLDER + str(uuid.uuid4()) + ".png"
     img = base64.b64decode(Base64_image.encode())
-    with open(plant_image_path, 'bw') as b64_buffer:
+    with open(plant_image_path, "bw") as b64_buffer:
         b64_buffer.write(img)
     return plant_image_path
