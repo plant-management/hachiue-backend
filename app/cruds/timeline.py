b14ce20b2ec0a18778b8c9a7cee18a03111310d6
hoimas <b061vdv@yamaguchi-u.ac.jp>
2021-12-16 16:23:33 +0000

    fix timelineのcrudsで取得する際,Image-dbをouterjoinで結合

--- app/cruds/timeline.py
+++ app/cruds/timeline.py
@@ -12,11 +12,13 @@ async def get_timeline_deta(db: AsyncSession, user_id: str):
                 production_model.Plant.plant_name,
                 production_model.Data.comment,
                 production_model.Data.created_at,
+                production_model.Image.character_image_path,
             )
             .order_by(desc(production_model.Data.created_at))
             .filter(production_model.Data.user_id == user_id)
             .filter(production_model.Plant.user_id == user_id)
             .outerjoin(production_model.Plant)
+            .outerjoin(production_model.Image.user_id == user_id)
             .limit(10)
         )
     )
@@ -24,13 +26,13 @@ async def get_timeline_deta(db: AsyncSession, user_id: str):
 
 
 # imageテーブルからcharacter_image_pathを取得(user_idで10件)
-async def get_ten_character_image(db: AsyncSession, user_id: str):
-    result: Result = await (
-        db.execute(
-            select(production_model.Image.character_image_path)
-            .order_by(desc(production_model.Image.created_at))
-            .filter(production_model.Image.user_id == user_id)
-            .limit(10)
-        )
-    )
-    return result.all()
+# async def get_ten_character_image(db: AsyncSession, user_id: str):
+#     result: Result = await (
+#         db.execute(
+#             select(production_model.Image.character_image_path)
+#             .order_by(desc(production_model.Image.created_at))
+#             .filter(production_model.Image.user_id == user_id)
+#             .limit(10)
+#         )
+#     )
+#     return result.all()
