ee9e4e723908601e2ba4de4c5b7f2c236ec41db4
hoimas <b061vdv@yamaguchi-u.ac.jp>
2021-12-16 18:38:07 +0000

    update timelineへ送信データ等のcrud操作をouterjoinして取得するように更新

--- app/cruds/timeline.py
+++ app/cruds/timeline.py
@@ -17,22 +17,16 @@ async def get_timeline_deta(db: AsyncSession, user_id: str):
             .order_by(desc(production_model.Data.created_at))
             .filter(production_model.Data.user_id == user_id)
             .filter(production_model.Plant.user_id == user_id)
-            .outerjoin(production_model.Plant)
-            .outerjoin(production_model.Image.user_id == user_id)
+            .filter(production_model.Image.user_id == user_id)
+            .outerjoin(
+                production_model.Plant,
+                production_model.Plant.plant_id == production_model.Data.plant_id,
+            )
+            .outerjoin(
+                production_model.Image,
+                production_model.Image.image_id == production_model.Data.image_id,
+            )
             .limit(10)
         )
     )
     return result.all()
-
-
-# imageテーブルからcharacter_image_pathを取得(user_idで10件)
-# async def get_ten_character_image(db: AsyncSession, user_id: str):
-#     result: Result = await (
-#         db.execute(
-#             select(production_model.Image.character_image_path)
-#             .order_by(desc(production_model.Image.created_at))
-#             .filter(production_model.Image.user_id == user_id)
-#             .limit(10)
-#         )
-#     )
-#     return result.all()
