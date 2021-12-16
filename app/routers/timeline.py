c657846126ae5ec023bf355b16bc8b633d82d6c6
hoimas <b061vdv@yamaguchi-u.ac.jp>
2021-12-16 18:40:43 +0000

    fix timelineのデータ取得操作を1回でできるよう修正

--- app/routers/timeline.py
+++ app/routers/timeline.py
@@ -10,16 +10,15 @@ router = APIRouter()
 # [タイムライン用]ユーザーのデータを１０件分返す
 @router.get("/timeline/{user_id}")
 async def send_timeline(user_id: str, db: AsyncSession = Depends(get_db)):
-    img = await timeline_cruds.get_ten_character_image(db, user_id=user_id)
+    # img = await timeline_cruds.get_ten_character_image(db, user_id=user_id)
     data = await timeline_cruds.get_timeline_deta(db, user_id=user_id)
     timeline_data = []
-    counter = 0
     # 画像のbase64 とその他のデータを辞書型で結合したい。もっといい書き方はありそう。
-    for i in img:
+    for i in data:
         character_image = png_to_base64(i["character_image_path"])
-        plant_name = data[counter]["plant_name"]
-        comment = data[counter]["comment"]
-        created_at = data[counter]["created_at"]
+        plant_name = i["plant_name"]
+        comment = i["comment"]
+        created_at = i["created_at"]
         timeline_data.append(
             {
                 "character_image": character_image,
@@ -28,5 +27,4 @@ async def send_timeline(user_id: str, db: AsyncSession = Depends(get_db)):
                 "created_at": created_at,
             }
         )
-        counter += 1
     return timeline_data
